065828bdd2f13e7e906d135a92fcd7521736dca2
Mikołaj Mielewczyk <mikolajmielewczyk@wp.pl>
2025-04-01 23:48:12 +0200

    Unet architecture

---
+++ models/unet.py
@@ -0,0 +1,107 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+    def forward(self, x):
+        return self.double_conv(x)
+
+class Down(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(Down, self).__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super(Up, self).__init__()
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels // 2, in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels, out_channels)
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+    def forward(self, x):
+        return self.conv(x)
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes, bilinear=True, time_emb_dim=128):
+        """
+        n_channels: Number of input channels (sketch (1) + noisy photo (3) = 4)
+        n_classes: Number of output channels (here 3 for predicting noise for the photo)
+        """
+        super(UNet, self).__init__()
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.up2 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, n_classes)
+        
+        # Time embedding for diffusion conditioning
+        self.time_mlp = nn.Sequential(
+            nn.Linear(1, time_emb_dim),
+            nn.ReLU(),
+            nn.Linear(time_emb_dim, time_emb_dim)
+        )
+        self.time_conv = nn.Conv2d(time_emb_dim, 64, kernel_size=1)
+
+    def forward(self, x, t):
+        """
+        x: input tensor of shape (B, 4, H, W) [concatenated sketch (1) + noisy photo (3)]
+        t: tensor of shape (B,) representing diffusion timesteps
+        """
+        # Process time embedding
+        t = t.unsqueeze(1).float()           # shape: (B, 1)
+        time_emb = self.time_mlp(t)            # shape: (B, time_emb_dim)
+        time_emb = time_emb[:, :, None, None]  # shape: (B, time_emb_dim, 1, 1)
+        time_emb = self.time_conv(time_emb)    # shape: (B, 64, 1, 1)
+        time_emb = time_emb.expand(-1, -1, x.shape[2], x.shape[3])  # shape: (B, 64, H, W)
+        
+        # Process input through initial conv block
+        x1 = self.inc(x)  # now x1 is (B, 64, H, W)
+        # Add time embedding to the feature map
+        x1 = x1 + time_emb
+        
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
